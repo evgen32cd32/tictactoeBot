4d8ba0d0033905b9c32c52faeeb36aac4a321baf
Evgeniy Khvatov <evgen32cd32@gmail.com>
2022-12-03 14:01:41 +0300

    Some experiments with html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import telepot
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 import json
 
 app = Flask(__name__)
@@ -10,8 +11,17 @@ with open('configs/env.json','r') as f:
 token = cfg['TOKEN']
 secret = cfg['SECRET']
 
-bot = telepot.Bot(token)
-bot.setWebhook("https://f4q73p.deta.dev/{}".format(secret), max_connections=1)
+#bot = telepot.Bot(token)
+#bot.setWebhook("https://f4q73p.deta.dev/{}".format(secret), max_connections=1)
+
+@app.route('/')
+def home():
+   return render_template('index.html',grid = [['X','O'],['Y'],['N']])
+
+@app.route('/action', methods={"POST"})
+def player_action():
+    print(request.form)
+    return "OK"
 
 @app.route('/{}'.format(secret), methods=["POST"])
 def telegram_webhook():
@@ -19,5 +29,11 @@ def telegram_webhook():
     if "message" in update:
         text = update["message"]["text"]
         chat_id = update["message"]["chat"]["id"]
-        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        #bot.sendMessage(chat_id, "From the web: you said '{}'".format(text),reply_markup=ReplyKeyboardMarkup(
+        #    keyboard=[[KeyboardButton(text='Yes'),KeyboardButton(text='No')],
+        #    [KeyboardButton(text='Left'),KeyboardButton(text='Right')]]))
     return "OK"
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
