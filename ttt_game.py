b4fc3e776892b738e3d8802cc0b15979ede25481
Evgeniy Khvatov <evgen32cd32@gmail.com>
2023-02-10 15:52:46 +0300

    Implemented new state system in ttt_* engine

--- ttt_game.py
+++ ttt_game.py
@@ -1,96 +1,54 @@
-from ttt_state import State, stateCanonForm
+from ttt_state import CanonState, stateCanonForm, printableState, Transform
 from ttt_bot import Bot
 
 def init_states():
     state_dict = {}
-    state_dict[0] = {0:State('         ',0)}
+    state_dict[0] = {0:CanonState('         ',0)}
     for i in range(1,10):
         state_dict[i] = {}
         p = 'O' if (i % 2) == 0 else 'X'
-        for __,st in state_dict[i-1].items():
+        for st in state_dict[i-1].values():
             if st.winner is None:
                 for j in range(9):
                     if st.s[j] == ' ':
                         ns = st.s[:j]+p+st.s[j+1:]
-                        score, cForm, _ = stateCanonForm(ns)
-                        if score in state_dict[i]:
-                            st.children.add(state_dict[i][score])
-                        else:
-                            nst = State(cForm, score)
+                        score, cForm, transform = stateCanonForm(ns)
+                        if score not in state_dict[i]:
+                            nst = CanonState(cForm, score)
                             state_dict[i][score] = nst
-                            st.children.add(nst)
-    return state_dict[0][0]
+                        else:
+                            nst = state_dict[i][score]
+                        if nst not in st.children:
+                            st.children[nst] = []
+                        st.children[nst].append(transform)
+    return {k:v for d in state_dict.values() for k,v in d.items()}
 
-def get_user_action(st: State):
-    c = ''
-    while (not c.isdigit()) or int(c) == 9:
-        c = input(st)
-    player = 'O' if (st.s.count(' ') % 2) == 0 else 'X'
-    ns = st.s[0:int(c)] + player + st.s[int(c)+1:]
-    for nst in st.children:
-        if nst.s == ns:
-            return nst
-    print('cell is not empty')
-    return get_user_action(st)
-
-def _recursive_save(st: State, b: Bot, saved: set, f):
-    if st.score not in saved:
-        saved.add(st.score)
-        f.write('{};{};{};{}\n'.format(st.score,st.s,b.d[st],[sc.score for sc in st.children]))
-        for sc in st.children:
-            _recursive_save(sc,b,saved,f)
-
-def save_game(st: State, b: Bot, file = 'configs/svst_v2.csv'):
+def save_game(state_dict: dict, b: Bot, file = 'configs/svst_v2.csv'):
     with open(file,'w') as f:
-        saved = set()
-        _recursive_save(st,b,saved,f)
+        [f.write(f'{k};{b.d[v]}\n') for k,v in state_dict.items()]
 
-
-def load_game(file = 'configs/svst_v2.csv'):
-    #assert()
-    head = None
-    b = None
-    d = {}
-    bot_d = {}
+def load_game(state_dict: dict, file: str = 'configs/svst_v2.csv'):
     with open(file,'r') as f:
-        for line in f.readlines():
-            split = line.split(';')
-            id = int(split[0])
-            st = State(split[1],id)
-            ar = split[3]
-            ar = ar[1:-2]
-            ar = ar.split(',')
-            if ar[0] == '':
-                ar = []
-            d[id] = (st,[int(x) for x in ar])
-            bot_d[st] = float(split[2])
-            #if bot_d[st] != 0.5 and bot_d[st] != 1.0:
-            #    print(line)
-    head = d[0][0]
-    b = Bot(head,bot_d)
-    for _, (st, ch_ar) in d.items():
-        for sc_id in ch_ar:
-            st.children.add(d[sc_id][0])
-    print("Loaded successfully")
-    return (head, b)
+        return Bot(state_dict, {int(line.split(';')[0]):float(line.split(';')[1]) for line in f.readlines()})
 
 if __name__ == '__main__':
+    state_dict = init_states()
     try:
-        (start,b) = load_game()
+        b = load_game(state_dict)
+        print("Loaded successfully")
     except Exception:
         print("Can't load. New initialization")
-        start = init_states()
-        b = Bot(start,expl_rate=0.5)
-    ca = start
-    plf = True
+        b = Bot(state_dict)
+    ca = state_dict[0]
+    plf = 'X'
+    trans = Transform(0,1)
+    prev = {'X':None, 'O':None}
     while ca.winner is None:
-        if plf:
-            #ca = get_user_action(ca)
-            ca = b.action(ca)
-        else:
-            ca = b.action(ca)
-        print(ca)
-        plf = not plf
-    b.get_winner(ca.winner)
-    save_game(start, b)
+        ca, tr = b.action(ca,prev[plf])
+        prev[plf] = ca
+        trans += tr
+        print(printableState(ca.getTransformed(trans)))
+        plf = 'X' if plf == 'O' else 'O'
+    b.get_winner(ca.winner, prev)
+    save_game(state_dict, b)
     print(ca.winner)
--- ttt_state.py
+++ ttt_state.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 # '012345678'
 #
 # 0|1|2
@@ -5,6 +7,30 @@
 # 7|8|3
 # -+-+-
 # 6|5|4
+
+#Transform = namedtuple('Transform',['corner','direction'])
+class Transform:
+    def __init__(self, corner, direction):
+        assert corner in [0,2,4,6], 'Wrong corner'
+        assert direction in [-1,1], 'Wrong direction'
+        self.corner = corner
+        self.direction = direction
+    
+    def reverse(self):
+        if self.direction == 1:
+            if self.corner == 2:
+                self.corner = 6
+            elif self.corner == 6:
+                self.corner = 2
+        return self
+    
+    def __add__(self, other):
+        return Transform((self.corner + self.direction*other.corner + 8)%8, self.direction * other.direction)
+    
+    def __iadd__(self, other):
+        self.corner = (self.corner + self.direction*other.corner + 8)%8
+        self.direction *= other.direction
+        return self
 
 def charScore(ch):
     if ch == 'X':
@@ -21,26 +47,27 @@ def stateCanonForm(state: str):
     corners = [charScore(x) for x in state[:7:2]]
     # (corner_id, clockwise)
     # (0, 1) - Canonical form already
-    potential = [(i*2,1) for i, x in enumerate(corners) if x == max(corners)]
-    potential.extend([(x,-1) for x,_ in potential])
+    potential = [Transform(i*2,1) for i, x in enumerate(corners) if x == max(corners)]
+    potential.extend([Transform(x.corner,-1) for x in potential])
     for i in range(1,8):
-        scores = [charScore(state[(x + a*i)%8]) for x,a in potential]
+        scores = [charScore(state[(x.corner + x.direction * i)%8]) for x in potential]
         inds = [i for i, x in enumerate(scores) if x == max(scores)]
         if len(inds) == 1:
-            bestTransform = potential[inds[0]]
             break
         potential = [potential[ind] for ind in inds]
-    if bestTransform is None:
-        bestTransform = potential[0]
+    bestTransform = potential[0]
     state8 = state[:-1]
-    canonForm = state8[bestTransform[0]::bestTransform[1]] + state8[:bestTransform[0]:bestTransform[1]] + state[-1]
-    return (calcStateScore(canonForm), canonForm, bestTransform)
+    canonForm = state8[bestTransform.corner::bestTransform.direction] + state8[:bestTransform.corner:bestTransform.direction] + state[-1]
+    return (calcStateScore(canonForm), canonForm, bestTransform.reverse())
 
-class State:
+def printableState(state: str):
+    return state[0]+'|'+state[1]+'|'+state[2]+'\n-+-+-\n'+state[7]+'|'+state[8]+'|'+state[3]+'\n-+-+-\n'+state[6]+'|'+state[5]+'|'+state[4] + '\n'
+
+class CanonState:
     def __init__(self, s:str, score: int):
         self.score = score
         self.s = s
-        self.children = set()
+        self.children = {}
         turn = 9 - self.s.count(' ')
         player = 'X' if (turn % 2) == 1 else 'O'
         if (       self.s[0:3] == player*3
@@ -52,11 +79,14 @@ class State:
                 or self.s[0] + self.s[8] + self.s[4] == player*3
                 or self.s[6] + self.s[8] + self.s[2] == player*3):
             self.winner = player
-            #print(self.s[0]+'|'+self.s[1]+'|'+self.s[2]+'\n-+-+-\n'+self.s[7]+'|'+self.s[8]+'|'+self.s[3]+'\n-+-+-\n'+self.s[6]+'|'+self.s[5]+'|'+self.s[4])
         elif turn == 9:
             self.winner = 'D'
         else:
             self.winner = None
     
+    def getTransformed(self, tr: Transform):
+        state8 = self.s[:-1]
+        return state8[tr.corner::tr.direction] + state8[:tr.corner:tr.direction] + self.s[-1]
+
     def __str__(self):
-        return self.s[0]+'|'+self.s[1]+'|'+self.s[2]+'\n-+-+-\n'+self.s[7]+'|'+self.s[8]+'|'+self.s[3]+'\n-+-+-\n'+self.s[6]+'|'+self.s[5]+'|'+self.s[4] + '\n'
+        return printableState(self.s)
